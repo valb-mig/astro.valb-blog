e356b5fd86eb260fc7fb6067e8b6069bb6f9f669
valb-mig <valb.mig@gmail.com>
2026-07-08 09:14:26 -0300

    chore: adiciona Tailwind, React e shadcn/ui ao projeto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://fake', // TODO: update before deploying
   output: 'server',
   adapter: vercel(),
-  integrations: [icon()],
+  integrations: [icon(), react()],
+  vite: { plugins: [tailwindcss()] },
 });
